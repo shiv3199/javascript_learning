9bd940d6483d470665cd87ee5ad83f6c6c8328b9
shiv3199 <shivam.maurya.3199@gmail.com>
2024-03-25 20:22:31 +0000

    data_type

---
+++ js_basics/02_datatype.js
@@ -0,0 +1,12 @@
+//in javascript we have two data type
+//primitive and non primitive
+//number 1,2
+//bigint 
+//string ""
+//boolean true/flase
+//null empty
+//undefined not defined the value
+//symbol unique
+
+//non primitive
+//object
