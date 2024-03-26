0117e19065f6e237512211cd3ec01d7094bb8c0b
shiv3199 <shivam.maurya.3199@gmail.com>
2024-03-26 19:53:58 +0000

    data type summary

--- js_basics/03_conversionOperation.js
+++ js_basics/03_conversionOperation.js
@@ -13,6 +13,54 @@ console.log(typeof scorestr)
 
 let bong = "56jk"
 let gong = Number(bong)
-console.log(typeof gong)
-console.log(gong)
+// console.log(typeof gong)
+// console.log(gong)
 //its give us number but real number is NaN 
+
+
+//****************opration */
+/**
+ this is 
+ */
+
+ /*in conversion if first is string so output is string 
+ and 
+ if first is number so first its do opration then add strng
+ ex : "2"+2 output is 22 bcoz first is string
+ 2+2+"2" output is 42 bcoz first its add num then add string
+ */
+
+ //assignment oprator =
+ //prefix increment oprator first increase then print number
+ //postfix first print number then increment the number
+
+ //**********************data type summary*******************
+ //two type 
+ //1 primitive(call by value)
+ /*
+ string
+ number
+ boolean
+ null //empty
+ undefined// var declear but value not define
+ symbol// unique value so we give symbol
+ bigint// biggest value
+
+  */
+
+
+
+
+ //2 non primitive(refrence type)
+ /**
+  * array
+  * objects
+  * function
+  */
+ const hero=["adma","mangu","changu"]
+ const man={
+    name:"raja",
+    age:28
+ }
+
+console.log(typeof hero);
